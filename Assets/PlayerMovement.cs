1b850e3c846706136a888a5e1ebfa482d8799c98
Atriel <filip.venera@seznam.cz>
2025-03-28 20:50:24 +0100

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+
+    private void Awake(){
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update(){
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+    }
+}
